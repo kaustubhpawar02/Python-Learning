329fb36c2fab843c2938134b1b2cee6e9fcb9556
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-06-23 14:47:28 +0530

    Code added

---
+++ Day_2/Addition.py
@@ -0,0 +1,12 @@
+print("Enter first number :")   #11
+
+no1 = input()       #no1 will be treated as string by default  
+
+print("Enter second number :")  #21
+
+no2 = input()      #no2 will be treated as string by default  
+
+
+Ans = no1+no2     # concatination of two str values
+
+print("Addition is :", Ans)  # 1121
---
+++ Day_2/AdditionX.py
@@ -0,0 +1,12 @@
+print("Enter first number : ")
+No1 = input()
+
+print("Enter second number : ")
+No2 = input()
+
+print(type(No1))   #str
+print(type(No2))   #str
+
+Ans = No1 + No2
+
+print("Addition is : ",Ans) 
