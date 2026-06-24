4febdf6b45878e74e9fd66e99cce960e634d6ed5
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-06-24 15:19:50 +0530

    Function1.py added

---
+++ Day_4/Function1.py
@@ -0,0 +1,8 @@
+def Addition():
+    a = int(input("Enter 1st number :\n"))
+    b = int(input("Enter 2nd number :\n"))
+    ans = a + b
+    print('addition is :',ans)
+
+if __name__ == "__main__":
+    Addition()
