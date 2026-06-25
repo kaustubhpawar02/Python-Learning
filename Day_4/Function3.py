e6f561f6359edf5b8963cb9b2cd7eafdf52e1a66
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-06-25 22:09:52 +0530

    code added

---
+++ Day_4/Function3.py
@@ -0,0 +1,18 @@
+# def main():
+
+#     print("Enter first number : ")
+    
+#     valu1 = int(input())
+
+#     print("Enter second number :")
+
+
+#     valu2 = int(input())
+
+#     ret = Addtion(valu1,valu2)   #Error
+
+#     print("Addition is : ",ret)
+
+# if __name__ == "__main__":
+#     main()
+
