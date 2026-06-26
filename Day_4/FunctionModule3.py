e840c42680c518da4a34f250004529760fd30272
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-06-26 19:46:06 +0530

    code added

---
+++ Day_4/FunctionModule3.py
@@ -0,0 +1,14 @@
+from Marvellous import Addition
+
+def main():
+
+    value1 = int(input("Enter first number :"))
+
+    value2 = int(input("Enter second number :"))
+
+    ret = Addition(value1,value2)
+
+    print("Addition is :",ret)
+    
+if __name__ == "__main__":
+    main()
