7dbee175b55bca68dc22182c3e995d9f0bdabc00
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-06-26 19:37:36 +0530

    import module

---
+++ Day_4/FunctionModule1.py
@@ -0,0 +1,13 @@
+import Marvellous
+
+def main():
+    value1 = int(input("Enter  number1 :"))
+
+    value2 = int(input("Enter number2 : "))
+
+    ret = Marvellous.Addition(value1,value2)
+
+    print ("Addition is :",ret)
+
+if __name__ == "__main__":
+    main()
---
+++ Day_4/FunctionModule2.py
@@ -0,0 +1,13 @@
+import Marvellous as MI
+
+def main():
+
+    No1 = int(input("Enter a number 1 :"))
+    No2 = int(input("Enter a number 2 :"))
+
+    ret = MI.Addition(No1,No2)
+
+    print("Addition is :",ret)
+
+if __name__ == "__main__":
+    main()
