f2fa9c34fbe5ee956c8ecd9699b6ca76c08565b4
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-06-27 09:52:16 +0530

    Code Added

---
+++ Day_5/KeywordParam.py
@@ -0,0 +1,11 @@
+def Area(Radius,PI):
+    Ans = PI*Radius*Radius
+    return Ans
+
+def main():
+    
+    ret =Area(Radius=12.5,PI=3.14)
+    print("Area of Circle is :",ret)
+
+if __name__ == "__main__":
+    main()
