6af9c8bd40ac16780c161eb44a677741ae088db4
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-07-01 14:10:48 +0530

    Code Added

---
+++ Day_6/IterableListSummationX.py
@@ -0,0 +1,13 @@
+def main():
+
+    Marks = [11,21,51,101]
+
+    Sum = 0 
+
+    for i in Marks:
+        Sum = Sum + i
+    
+    print("Addition of Numbers in list is ",Sum)
+
+if __name__ == "__main__":
+    main()
